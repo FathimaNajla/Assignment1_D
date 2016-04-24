9436475487cc800e237f7afd5b1bf4ad6601b34d
Fathima <najla.dsc@gmail.com>
2016-04-23 21:42:11 -0700

    adding fnOne

---
+++ SomeClass.java
@@ -0,0 +1,13 @@
+public class SomeClass
+{
+	public void fnOne()
+	{
+		System.out.println("I couldnt repair brakes");
+	}
+	
+	public static void main(String[] args)
+	{
+		SomeClass sc = new SomeClass();
+		sc.fnOne();
+	}
+}
