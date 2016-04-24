2c48a02861d1d8dd6450e2fc97a8a77dd9b99646
Fathima <najla.dsc@gmail.com>
2016-04-23 22:00:45 -0700

    Add multiple events

--- SomeClass.java
+++ SomeClass.java
@@ -1,20 +1,28 @@
+
 public class SomeClass
 {
+	private ArrayList<String> events = new ArrayList<String>();
+	public void createEvents() {
+		events.add("We're going to a movie on Saturday");
+		events.add("Study session on Sunday - Jim's house");
+	}
+
 	public void thoughtForTheDay()
 	{
 		System.out.println("on your hand you have different fingers");
 	}
 
-	public void upcomingEvents()
-	{
-		System.out.println("upcoming events");
-		System.out.println("party at janeds");
+	public void upcomingEvents() {
+		System.out.println("Upcoming Events");
+		for (String event : events)
+			System.out.println(event);
 	}
 	
 	public static void main(String[] args)
 	{
 		SomeClass sc = new SomeClass();
 		sc.fnOne();
+		sc.createEvents();
 		sc.upcomingEvents();
 	}
 }
